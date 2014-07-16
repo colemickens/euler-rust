f5e9105564c9d21117437491c418551be013d677
Cole Mickens <cole.mickens@gmail.com>
2014-07-16 15:05:55 -0700

    solved problem7

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+// gives the prime factorization for a number
 pub fn prime_factors(number: uint) -> Vec<uint> {
   let mut number = number;
   let mut factors = vec!();
@@ -7,12 +8,14 @@ pub fn prime_factors(number: uint) -> Vec<uint> {
   while number > 1 {
     while number % test == 0 {
       factors.push(test);
+      println!("found: {}", test);
       number = number / test;
     }
     test = test + 1;
     if test * test > number {
       if number > 1 {
         factors.push(number);
+        println!("found: {}", test);
         break;
       }
     }
@@ -20,11 +23,9 @@ pub fn prime_factors(number: uint) -> Vec<uint> {
   factors
 }
 
-// pub fn count_factors(foo: Vec<uint>) -> Vec<(uint, uint)> {
-
+// count the factors in a sorted list and return a hashmap
 pub fn count_factors(foo: &Vec<uint>) -> HashMap<uint, uint> {
   let mut count_map = HashMap::new();
-  let mut count_list = vec!();
 
   // credit to kwantam in #rust
   let last = foo.iter().fold((None, 0), |(p, l): (Option<uint>, uint), &nxt| {
@@ -35,7 +36,6 @@ pub fn count_factors(foo: &Vec<uint>) -> HashMap<uint, uint> {
                 (p, l + 1)
             } else {
                 count_map.insert(s, l);
-                count_list.push((s, l));
                 (Some(nxt), 1)
             }
         }
@@ -45,10 +45,43 @@ pub fn count_factors(foo: &Vec<uint>) -> HashMap<uint, uint> {
       (None, _) => (),
       (Some(prime), count) => {
         count_map.insert(prime, count);
-        count_list.push((prime, count));
       },
   };
-
-  // count_list
   count_map
+}
+
+// An iterator that yields prime numbers
+pub struct PrimeIter {
+  counter: uint,
+  found_primes: Vec<uint>,
+}
+
+impl PrimeIter {
+  pub fn new() -> PrimeIter {
+    PrimeIter{
+      counter: 2u,
+      found_primes: vec!(),
+    }
+  }
+}
+
+impl Iterator<uint> for PrimeIter {
+  fn next(&mut self) -> Option<uint> {
+    loop {
+        let mut temp = 2u;
+        while temp * temp <= self.counter {
+          if self.counter % temp == 0 {
+            break;
+          }
+          temp = temp + 1;
+        }
+        if temp * temp > self.counter {
+          let found_prime = self.counter;
+          self.found_primes.push(found_prime);
+          self.counter = self.counter + 1;
+          return Some(found_prime);
+        }
+      self.counter = self.counter + 1;
+    }
+  }
 }
--- tests/problem007.rs
+++ tests/problem007.rs
@@ -3,11 +3,15 @@ Problem 007
 
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-What is the 10 001st prime number?
+What is the 10_001st prime number?
 */
 
+extern crate euler;
+use euler::{PrimeIter};
+
 fn problem007() -> uint {
-  104743
+  let mut prime_iter = PrimeIter::new();
+  prime_iter.nth(10000).unwrap()
 }
 
 #[test]
