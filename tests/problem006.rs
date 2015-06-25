0e69b035449749ce3122fd8da861efca21e660d7
Cole Mickens <cole.mickens@gmail.com>
2015-06-24 18:52:29 -0700

    fix, enable problem006

--- tests/problem006.rs
+++ tests/problem006.rs
@@ -6,25 +6,22 @@ The sum of the squares of the first ten natural numbers is,
 
 The square of the sum of the first ten natural numbers is,
       (1 + 2 + ... + 10)^2 = 55^2 = 3025
-      
+
 Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 */
 
-#![feature(globs)]
-use std::num;
-use std::iter::AdditiveIterator;
+#![feature(iter_arith)]
 
-fn problem006(num_to_take: uint) -> uint {
-  let square_of_sum = num::pow(range(1u, num_to_take+1).sum(), 2);
-  let sum_of_squares = range(1u, num_to_take+1).map(|n| num::pow(n, 2)).sum();
-
+fn problem006(num_to_take: u32) -> u32 {
+  let square_of_sum = (1u32..num_to_take+1).sum::<u32>().pow(2);
+  let sum_of_squares = (1u32..num_to_take+1).map(|n| n.pow(2)).sum::<u32>();
   square_of_sum - sum_of_squares
 }
 
 #[test]
 fn test_problem006() {
-  assert_eq!(problem006(10), 2640)
-  assert_eq!(problem006(100), 25164150)
+  assert_eq!(problem006(10), 2640);
+  assert_eq!(problem006(100), 25164150);
 }
