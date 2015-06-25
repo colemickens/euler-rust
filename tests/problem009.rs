fd3cd6c0931254953d604eada4be62823bd0c6a8
Cole Mickens <cole.mickens@gmail.com>
2015-06-24 20:19:24 -0700

    fix, enable problem009

--- tests/problem009.rs
+++ tests/problem009.rs
@@ -4,27 +4,26 @@ Problem 009
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
         a^2 + b^2 = c^2
-        
+
 For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 */
 
-use std::num::pow;
-use std::num::from_int;
+fn problem009(target: u32) -> Option<u64> {
+  // TODO(colemickens): this is _very_ brutish
 
-fn problem009(target: uint) -> Option<uint> {
-  // _very_ brutish
-  for a in range(0u, target) {
-    for b in range(0u, a) {
-      let c_squared: f64 = from_int(pow(a, 2u) as int + pow(b, 2u) as int).unwrap();
+  let target = target as u64;
+  for a in (0u64..target) {
+    for b in (0u64..a) {
+      let c_squared: f64 = (a.pow(2) + b.pow(2)) as f64;
       let c = c_squared.sqrt();
-      if c.fract() > Float::epsilon() {
+      if c.fract() > std::f64::EPSILON {
         continue;
       }
-      
-      let c = c as uint;
+
+      let c = c as u64;
       let attempt = a + b + c;
       if attempt == target {
         return Some(a * b * c);
